b42777711565f6cedbe3663bae6dde00f4142b59
techybolek <techy_bolek@yahoo.com>
2025-05-20 17:36:06 -0500

    feature: netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,20 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), sitemap(), tailwind()],
   server: { port: 3000 },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   experimental: {
     session: true
+  },
+  vite: {
+    ssr: {
+      noExternal: ['path-to-regexp']
+    }
   }
 });
